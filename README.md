58bb2b465f6a954d9690b8fbab68991e287ee82c
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-03-15 16:01:50 -0500

    feat: :tada: Starting Javascript project
    Initializing the configuration for the Javascript project
    
    docs: :memo: Videos in the readme
    
    Desktop and mobile version videos in the readme.

---
+++ README.md
@@ -0,0 +1,8 @@
+
+https://github.com/JuanManuelGilQuiroga/proyectoWeb/assets/157772473/dadb9215-7d5f-4bb8-8bea-4b409388257c
+
+
+
+https://github.com/JuanManuelGilQuiroga/proyectoWeb/assets/157772473/71039496-70c2-4881-a665-9a040b9ca67d
+
+
---
+++ js/main.js
@@ -0,0 +1,107 @@
+const toggleMenuElement = document.getElementById("toggle__menu");
+const mainMenuElement = document.getElementById("header__nav")
+const linkMenuElement = document.getElementById("link__1")
+const link2MenuElement = document.getElementById("link__2")
+const link3MenuElement = document.getElementById("link__3")
+const link4MenuElement = document.getElementById("link__4")
+
+const carritoTextoElement = document.getElementById("carrito__vacio")
+const btnVaciar = document.getElementById("button__vaciar")
+const carritoListElement = document.getElementById("carrito__main__list")
+const totalElement = document.getElementById("total")
+const btnComprar = document.getElementById("button__comprar")
+
+const mainElement = document.getElementById("main")
+
+
+
+toggleMenuElement.addEventListener("click", ()=>{
+    setTimeout(()=>{mainMenuElement.classList.toggle("header__nav__show")}, 1000);
+    mainElement.classList.add("main__z__index");
+    
+})
+linkMenuElement.addEventListener("click", ()=>{
+    mainMenuElement.classList.remove("header__nav__show");
+    mainElement.classList.remove("main__z__index")
+})
+link2MenuElement.addEventListener("click", ()=>{
+    mainMenuElement.classList.remove("header__nav__show");
+    mainElement.classList.remove("main__z__index")
+})
+link3MenuElement.addEventListener("click", ()=>{
+    mainMenuElement.classList.remove("header__nav__show");
+    mainElement.classList.remove("main__z__index")
+})
+link4MenuElement.addEventListener("click", ()=>{
+    mainMenuElement.classList.remove("header__nav__show");
+    mainElement.classList.remove("main__z__index")
+})
+
+// boton menu
+
+const botonMain = document.getElementById("rayas");
+botonMain.addEventListener("click", () => {
+    botonMain.classList.add("bx-tada");
+    setTimeout(() => {
+        botonMain.classList.remove("bx-tada")
+    }, 1000);
+})
+btnVaciar.addEventListener("click", ()=>{
+    carritoTextoElement.classList.remove("eliminar__elemento");
+    carritoListElement.classList.add("eliminar__elemento");
+    totalElement.classList.add("eliminar__elemento");
+    document.getElementById("contador").innerText = 0;
+
+})
+btnComprar.addEventListener("click", ()=>{
+    carritoTextoElement.classList.remove("eliminar__elemento");
+    carritoListElement.classList.add("eliminar__elemento");
+    totalElement.classList.add("eliminar__elemento");
+    document.getElementById("contador").innerText = 0;
+
+})
+
+function contadorCarrito(id) {
+    let contador = parseInt(document.getElementById(id).innerText);
+    contador++;
+    document.getElementById(id).innerText = contador;
+}
+
+// bton borrar
+const botonBorrar = document.getElementById('bx_boton');
+const botonBorrar2 = document.getElementById('bx_boton2');
+const botonBorrar3 = document.getElementById('bx_boton3');
+const totalCarrito = document.getElementById("total__carrito");
+
+botonBorrar.addEventListener("click", () => {
+    const nono = document.getElementById("nono");
+    nono.style.display = "none";
+    let contador = parseInt(document.getElementById("contador").innerText);
+    contador--;
+    document.getElementById("contador").innerText = contador;
+    let valorActual = parseFloat(totalCarrito.innerText.replace("$",""));
+    let nuevoValor = valorActual - 300000;
+    totalCarrito.innerText = "$" + nuevoValor;
+
+})
+botonBorrar2.addEventListener("click", () => {
+    const nono = document.getElementById("nono2");
+    nono.style.display = "none";
+    let contador = parseInt(document.getElementById("contador").innerText);
+    contador--;
+    document.getElementById("contador").innerText = contador;
+    let valorActual = parseFloat(totalCarrito.innerText.replace("$",""));
+    let nuevoValor = valorActual - 140000;
+    totalCarrito.innerText = "$" + nuevoValor;
+    
+})
+botonBorrar3.addEventListener("click", () => {
+    const nono = document.getElementById("nono3");
+    nono.style.display = "none";
+    let contador = parseInt(document.getElementById("contador").innerText);
+    contador--;
+    document.getElementById("contador").innerText = contador;
+    let valorActual = parseFloat(totalCarrito.innerText.replace("$",""));
+    let nuevoValor = valorActual - 90000;
+    totalCarrito.innerText = "$" + nuevoValor;
+})
