734d9abe5c4846e58f7c3b8a575e348512ff3806
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-03-17 15:42:46 -0500

    fix: :bug: Dropdown menu
    Arrangements for proper functioning of the dropdown menu. Change in the javascript for open and close the dropdown menu when the user click an option or the icon.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
-const header__nav = document.querySelector("#header__nav");
-const open = document.querySelector("#open");
-const close = document.querySelector("#close");
+const toggleMenuElement = document.getElementById("toggle__menu");
+const mainMenuElement = document.getElementById("main__menu")
+const linkMenuElement = document.getElementById("link")
+const link2MenuElement = document.getElementById("link__2")
+const link3MenuElement = document.getElementById("link__3")
+const link4MenuElement = document.getElementById("link__4")
+const link5MenuElement = document.getElementById("link__5")
 
-open.addEventListener("click", () => {
-    header__nav.classList.add("visible");
+toggleMenuElement.addEventListener("click", ()=>{
+    mainMenuElement.classList.toggle("main__menu--show");
 })
-
-close.addEventListener("click", () => {
-    header__nav.classList.remove("visible");
+linkMenuElement.addEventListener("click", ()=>{
+    mainMenuElement.classList.remove("main__menu--show");
+})
+link2MenuElement.addEventListener("click", ()=>{
+    mainMenuElement.classList.remove("main__menu--show");
+})
+link3MenuElement.addEventListener("click", ()=>{
+    mainMenuElement.classList.remove("main__menu--show");
+})
+link4MenuElement.addEventListener("click", ()=>{
+    mainMenuElement.classList.remove("main__menu--show");
+})
+link5MenuElement.addEventListener("click", ()=>{
+    mainMenuElement.classList.remove("main__menu--show");
 })
