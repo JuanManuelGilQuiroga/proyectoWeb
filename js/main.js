0a8353f68ce739abcb081b9a969d7fbd542f5eb8
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-03-18 18:04:19 -0500

    refactor: :bug: Arrangementsin the header and icons
    Header fixed and some icons changed.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const toggleMenuElement = document.getElementById("toggle__menu");
-const mainMenuElement = document.getElementById("main__menu")
+const mainMenuElement = document.getElementById("main__nav")
 const linkMenuElement = document.getElementById("link")
 const link2MenuElement = document.getElementById("link__2")
 const link3MenuElement = document.getElementById("link__3")
@@ -7,20 +7,20 @@ const link4MenuElement = document.getElementById("link__4")
 const link5MenuElement = document.getElementById("link__5")
 
 toggleMenuElement.addEventListener("click", ()=>{
-    mainMenuElement.classList.toggle("main__menu--show");
+    mainMenuElement.classList.toggle("main__nav--show");
 })
 linkMenuElement.addEventListener("click", ()=>{
-    mainMenuElement.classList.remove("main__menu--show");
+    mainMenuElement.classList.remove("main__nav--show");
 })
 link2MenuElement.addEventListener("click", ()=>{
-    mainMenuElement.classList.remove("main__menu--show");
+    mainMenuElement.classList.remove("main__nav--show");
 })
 link3MenuElement.addEventListener("click", ()=>{
-    mainMenuElement.classList.remove("main__menu--show");
+    mainMenuElement.classList.remove("main__nav--show");
 })
 link4MenuElement.addEventListener("click", ()=>{
-    mainMenuElement.classList.remove("main__menu--show");
+    mainMenuElement.classList.remove("main__nav--show");
 })
 link5MenuElement.addEventListener("click", ()=>{
-    mainMenuElement.classList.remove("main__menu--show");
+    mainMenuElement.classList.remove("main__nav--show");
 })
