9ee46a6da754cf43d94c8ec6b8e8c3c8dc77a97a
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-03-19 20:24:00 -0500

    feat: :sparkles: Empty cart
    Implementation of javascript for empty car.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,11 @@ const linkMenuElement = document.getElementById("link__1")
 const link2MenuElement = document.getElementById("link__2")
 const link3MenuElement = document.getElementById("link__3")
 const link4MenuElement = document.getElementById("link__4")
+const carritoTextoElement = document.getElementById("carrito__vacio")
+const btnVaciar = document.getElementById("button__vaciar")
+const carritoListElement = document.getElementById("carrito__main__list")
+const totalElement = document.getElementById("total")
+const btnComprar = document.getElementById("button__comprar")
 
 toggleMenuElement.addEventListener("click", ()=>{
     setTimeout(()=>{mainMenuElement.classList.toggle("header__nav__show")}, 1000)
@@ -21,7 +26,18 @@ link3MenuElement.addEventListener("click", ()=>{
 link4MenuElement.addEventListener("click", ()=>{
     mainMenuElement.classList.remove("header__nav__show");
 })
+btnVaciar.addEventListener("click", ()=>{
+    carritoTextoElement.classList.remove("eliminar__elemento");
+    carritoListElement.classList.add("eliminar__elemento");
+    totalElement.classList.add("eliminar__elemento");
 
+})
+btnComprar.addEventListener("click", ()=>{
+    carritoTextoElement.classList.remove("eliminar__elemento");
+    carritoListElement.classList.add("eliminar__elemento");
+    totalElement.classList.add("eliminar__elemento");
+
+})
 // boton menu
 
 const botonMain = document.getElementById("rayas");
