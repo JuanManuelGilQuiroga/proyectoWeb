874f16d1c0a3e79fbbb8ca585f4f84a60fb9b5b6
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-03-20 16:47:52 -0500

    refactor: :lipstick: Design completed
    Design of all the sections completed.

--- js/main.js
+++ js/main.js
@@ -4,27 +4,35 @@ const linkMenuElement = document.getElementById("link__1")
 const link2MenuElement = document.getElementById("link__2")
 const link3MenuElement = document.getElementById("link__3")
 const link4MenuElement = document.getElementById("link__4")
+
 const carritoTextoElement = document.getElementById("carrito__vacio")
 const btnVaciar = document.getElementById("button__vaciar")
 const carritoListElement = document.getElementById("carrito__main__list")
 const totalElement = document.getElementById("total")
 const btnComprar = document.getElementById("button__comprar")
 
+const mainElement = document.getElementById("main")
+
 toggleMenuElement.addEventListener("click", ()=>{
-    setTimeout(()=>{mainMenuElement.classList.toggle("header__nav__show")}, 1000)
+    setTimeout(()=>{mainMenuElement.classList.toggle("header__nav__show")}, 1000);
+    mainElement.classList.add("main__z__index");
     
 })
 linkMenuElement.addEventListener("click", ()=>{
     mainMenuElement.classList.remove("header__nav__show");
+    mainElement.classList.remove("main__z__index")
 })
 link2MenuElement.addEventListener("click", ()=>{
     mainMenuElement.classList.remove("header__nav__show");
+    mainElement.classList.remove("main__z__index")
 })
 link3MenuElement.addEventListener("click", ()=>{
     mainMenuElement.classList.remove("header__nav__show");
+    mainElement.classList.remove("main__z__index")
 })
 link4MenuElement.addEventListener("click", ()=>{
     mainMenuElement.classList.remove("header__nav__show");
+    mainElement.classList.remove("main__z__index")
 })
 
 // boton menu
@@ -48,3 +56,4 @@ btnComprar.addEventListener("click", ()=>{
     totalElement.classList.add("eliminar__elemento");
 
 })
+
