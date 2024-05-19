1715771edd252a6d3b9ec3183bed91c64810f0f6
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-05-18 21:25:33 -0500

    feat: :sparkles: Component of the header completed

---
+++ src/js/components/header.js
@@ -0,0 +1,54 @@
+import { LitElement,html,css } from "lit";
+
+export class Header extends LitElement{
+
+    static properties = {
+        section: { type: String }
+    }
+
+    constructor(){
+        super();
+        this.section = localStorage.getItem("section") || "todosLosProductos";
+    }
+
+    static styles = css`
+    a{
+        text-decoration: none;
+    }
+    a:hover{
+        cursor: pointer;
+    }
+    .header__menu{
+        list-style: none;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-evenly;
+        background-color: var(--color-black);
+    }
+    .menu__item{
+        position: sticky;
+        color: var(--color-white);
+        font-family: "Staatliches";
+    }
+    `
+    changeSection(sectionChanged){
+        this.section = sectionChanged;
+        localStorage.setItem("section", this.section)
+    }
+
+    render(){
+        return html`
+        ${console.log(this.section)}
+        <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+        <ul class="header__menu" id="header__menu">
+                <li><a class="menu__item" id="link__1" @click=${()=>this.changeSection("todosLosProductos")}><i class='bx bxs-star'></i>Todos los productos</a></li>
+                <li><a class="menu__item" id="link__2" @click=${()=>this.changeSection("abrigos")}><i class='bx bxs-star'></i>Abrigos</a></li>
+                <li><a class="menu__item" id="link__3" @click=${()=>this.changeSection("camisetas")}><i class='bx bxs-star'></i>Camisetas</a></li>
+                <li><a class="menu__item" id="link__4" @click=${()=>this.changeSection("pantalones")}><i class='bx bxs-star'></i>Pantalones</a></li>
+        </ul>
+        `
+    }
+}
+customElements.define("my-header", Header)
